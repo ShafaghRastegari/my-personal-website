44537ef7a96387beaa405460fa49e91b63ffc900
shafaghrastegari <shafaghrastegari@gmail.com>
2021-10-16 00:05:23 +0330

    Updates

---
+++ precache-manifest.81a69d872a541a9f9f9a1c1f3f505204.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0109c403a003f20a4cc4dcac36940db1",
+    "url": "/my-personal-website/index.html"
+  },
+  {
+    "revision": "8e388b3f827f898a99d5",
+    "url": "/my-personal-website/static/css/main.de04c303.chunk.css"
+  },
+  {
+    "revision": "ba4f9a47fa590b1923fa",
+    "url": "/my-personal-website/static/js/2.43528144.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/my-personal-website/static/js/2.43528144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e388b3f827f898a99d5",
+    "url": "/my-personal-website/static/js/main.367c1e0d.chunk.js"
+  },
+  {
+    "revision": "58c0fad229775d95839e",
+    "url": "/my-personal-website/static/js/runtime-main.68361c2f.js"
+  },
+  {
+    "revision": "69498897ce3437235485447cfb6c6dd4",
+    "url": "/my-personal-website/static/media/Shafagh.69498897.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/my-personal-website/static/media/resume.9d3da0e2.pdf"
+  }
+]);
